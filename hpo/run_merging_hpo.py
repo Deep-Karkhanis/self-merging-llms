fab2d505fba35350f6d64cb01a428745e608adc2
crwhite14 <colin@realityengines.ai>
2024-03-05 15:55:53 -0800

    update hpo file

--- hpo/run_merging_hpo.py
+++ hpo/run_merging_hpo.py
@@ -1,18 +1,18 @@
 import optuna
 
 
-def create_mergekit_yaml(trial, 
+def create_mergekit_yaml(config_file_name, 
                          layer_length, 
                          layer_overlap, 
-                         base_model="cognitivecomputations/dolphin-2.2-70b",
-                         max_layers=80):
-    
-    config_file_name = "configs/MegaDolphin-Optuna-" + str(trial.number) + ".yaml"
+                         base_model,
+                         max_layers):
+
     step_size = int(layer_length * (1 - layer_overlap))
+    if step_size == 0:
+        step_size = 1
     layer_start_points = [i for i in range(0, max_layers-step_size, step_size)]
     layer_end_points = [i + layer_length for i in layer_start_points]
     layer_end_points[-1] = max_layers
-    print(layer_length, layer_overlap, step_size, layer_start_points, layer_end_points)
 
     yaml_config = """dtype: float16\nmerge_method: passthrough\nslices:"""
 
@@ -31,16 +31,28 @@ def objective(trial):
     layer_overlap = trial.suggest_float('layer_overlap', 0.0, 0.5)
 
     # create mergekit config
-    create_mergekit_yaml(trial, layer_length, layer_overlap)
+    config_file_name = "configs/MegaDolphin-Optuna-" + str(trial.number) + ".yaml"
+    create_mergekit_yaml(config_file_name, 
+                         layer_length, 
+                         layer_overlap,
+                         base_model="cognitivecomputations/dolphin-2.2-70b",
+                         max_layers=80)
 
-    # TODO: create model here, e.g.
-    #!mergekit-yaml config.yaml merge --copy-tokenizer --allow-crimes --out-shard-size 1B --lazy-unpickle
+    try:
+        # TODO: run mergekit command to create model here, given the config config_file_name, e.g. equivalent to
+        #!mergekit-yaml $config_file_name merge --copy-tokenizer --allow-crimes --out-shard-size 1B --lazy-unpickle
 
-    # TODO: evaluate model here
-    eval_loss = 1
+        # TODO: evaluate model here, and replace 0.5 with the true eval loss
+        eval_loss = 0.5
 
+    except Exception as e:
+        # if we fail to create and evaluate the model, then print the error and return the worst possible loss
+        print(e)
+        return 1.0
+
+    # return loss on the eval set
     return eval_loss
 
 
 study = optuna.create_study()
-study.optimize(objective, n_trials=500)
+study.optimize(objective, n_trials=50)
